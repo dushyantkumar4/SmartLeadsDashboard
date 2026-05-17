94450da69e250d6137da2e5fe8d3d933ccb2960c
dushyant kumar <dushyantvelar@gmail.com>
2026-05-17 17:10:31 +0530

    resolve the error for the role base access

--- backend/src/models/lead/lead.routes.ts
+++ backend/src/models/lead/lead.routes.ts
@@ -8,7 +8,6 @@ import {
   updateLead,
 } from "./lead.controller.js";
 import isLeadOwner from "../../middlewares/isOwner.js";
-import checkRole from "../../middlewares/roleBase.js";
 import { protect } from "../../middlewares/auth.middleware.js";
 import validate from "../../middlewares/validate.middleware.js";
 import {
@@ -20,7 +19,7 @@ const router = Router();
 
 router.post("/lead", protect, validate(createLeadValidation), createLead);
 
-router.get("/", protect, checkRole("admin"), getLeads);
+router.get("/", protect, getLeads);
 
 router.get("/:id", protect, isLeadOwner, getSingleLead);
 
