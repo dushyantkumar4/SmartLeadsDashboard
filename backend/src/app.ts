45d013fb59f464c8970343b928db1393629009b3
dushyant kumar <dushyantvelar@gmail.com>
2026-05-17 22:19:56 +0530

    added vercel file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 
-
 import authRoutes from "./models/auth/auth.routes.js";
 import leadRoutes from "./models/lead/lead.routes.js";
 
@@ -9,7 +8,17 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-frontend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
